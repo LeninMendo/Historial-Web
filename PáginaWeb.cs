95fa9231dccd5c8eca433b81ee741f631c79f1cd
Lenin Mendo <leninmendo1414@gmail.com>
2026-09-18 21:19:17 -0500

    Second commit

---
+++ PáginaWeb.cs
@@ -0,0 +1,23 @@
+﻿public class PaginaWeb
+{
+    private string url;
+    private string titulo;
+
+    public PaginaWeb(string url, string titulo)
+    {
+        this.url = url;
+        this.titulo = titulo;
+    }
+
+    public string Url
+    {
+        get { return url; }
+        set { url = value; }
+    }
+
+    public string Titulo
+    {
+        get { return titulo; }
+        set { titulo = value; }
+    }
+}
